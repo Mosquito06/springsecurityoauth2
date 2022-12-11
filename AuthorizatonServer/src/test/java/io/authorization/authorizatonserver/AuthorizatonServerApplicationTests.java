f00b7e98e49420e7afa392ab7e75d210c4711bee
chinchin <skykim10908@naver.com>
2022-12-11 16:12:34 +0900

    client-authorization-resource 실습

---
+++ AuthorizatonServer/src/test/java/io/authorization/authorizatonserver/AuthorizatonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.authorization.authorizatonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizatonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/Springsecurityoauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/Springsecurityoauth2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springsecurityoauth2Application {
-
-    public static void main(String[] args) {
+public class Springsecurityoauth2Application
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(Springsecurityoauth2Application.class, args);
     }
-
 }
